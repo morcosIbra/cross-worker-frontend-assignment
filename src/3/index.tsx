59fcb26b23405750fea828a60b2ff970774a7a55
morcos <mdanielibra@gmail.com>
2022-08-23 22:41:19 +0200

    implement task 3 of displaying filtered items

--- src/3/index.tsx
+++ src/3/index.tsx
@@ -2,15 +2,23 @@ import { FunctionComponent, useState } from "react";
 
 const Task3: FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
-  const [items, setItems] = useState([]);
-
+  const [items, setItems] = useState(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]);
+  
+  const inputChange = (e: any) => setInputValue(e.target.value);
+  const filteredItems = items.filter((item) => item.includes(inputValue));
+  
   return (
     <div>
       <label>Search Input: </label>
-      <input />
+      <input value={inputValue} onChange={inputChange} />
       <br />
       <br />
       #List goes here#
+      <ul>
+        {filteredItems.map((item) => {
+          return <li> {item} </li>;
+        })}
+      </ul>
     </div>
   );
 };
