41c6f8448d62dcb445b18358519a31ab0966ac63
morcos <mdanielibra@gmail.com>
2022-08-23 22:40:38 +0200

    implement task 2 of display input changes

--- src/2/index.tsx
+++ src/2/index.tsx
@@ -3,13 +3,15 @@ import { FunctionComponent, useState } from "react";
 const Task2: FunctionComponent = () => {
   const [value, setValue] = useState("");
 
+  const inputChange = (e: any) => setValue(e.target.value);
+
   return (
     <div>
       <label>Controlled Input: </label>
-      <input />
+      <input value={value} onChange={inputChange} />
       <br />
       <br />
-      <p>#Show Input value here#</p>
+      <p>#Show Input value here# {value}</p>
     </div>
   );
 };
